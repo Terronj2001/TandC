61060af85a694da3db484e8ebb55ec2eadad8dce
Terronj2001 <90802519+Terronj2001@users.noreply.github.com>
2025-01-04 04:40:49 +0000

    Co-authored-by: Cam <MontyMango@users.noreply.github.com>

---
+++ App.js
@@ -0,0 +1,17 @@
+// https://react.dev/learn
+
+export default function MyApp() {
+    return  (
+        <div>
+            <h1>This is a new app</h1>
+            <newButton/>
+        </div>
+    )
+}
+
+function newButton()    {
+    return (
+        <button>This is a button</button>
+    );
+}
+
